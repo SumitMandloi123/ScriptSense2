24bc3023219b3fc84d7d7ae4ba4e6c5aa0bbb48e
Sumit Mandloi <sumit.mandloi@unthinkable.co>
2025-03-12 17:10:29 +0530

    chages in pomFixtures

--- fixtures/pomFixture.ts
+++ fixtures/pomFixture.ts
@@ -1,25 +1,29 @@
-import { test as baseTest, chromium, Browser, Page, BrowserContext } from "@playwright/test";
+import { test as baseTest, BrowserContext, Page } from "@playwright/test";
 import { SignInPage } from "../tests/pages/signinPage";
 import { HomePage } from "../tests/pages/homePage";
 
+let context: BrowserContext;
+let page: Page;
 
-let page;
 type pages = {
     signinPage: SignInPage;
-    homePage: HomePage
-}
+    homePage: HomePage;
+};
 
 const testPages = baseTest.extend<pages>({
-    signinPage: async ({ browser,context}, use) => {
-    console.log("Launching browser...");
-    context = await browser.newContext({viewport: { width: 1920, height: 1080 }}); // One context for all tests
-    page = await context.newPage();
+    signinPage: async ({ browser }, use) => {
+        console.log("Launching browser...");
+        context = await browser.newContext({ viewport: { width: 1920, height: 1080 } });
+        page = await context.newPage(); // Initialize page
         await use(new SignInPage(page));
     },
-    homePage: async ({  }, use) => {
+    homePage: async ({}, use) => {
+        if (!page) {
+            throw new Error("Page is not initialized. Make sure 'signinPage' runs first.");
+        }
         await use(new HomePage(page));
     },
-})
+});
 
 export const test = testPages;
 export const expect = testPages.expect;
