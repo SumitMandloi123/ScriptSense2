a614bc7a57be500979b757f7a984d72631b9c258
Sumit Mandloi <sumit.mandloi@unthinkable.co>
2025-03-17 11:30:34 +0530

    added console.log in test

--- tests/features/createManualDispence.spec.ts
+++ tests/features/createManualDispence.spec.ts
@@ -3,10 +3,12 @@ import testData from "../../resources/data/testData.json";
 
 test.describe("Login and create a manual dispense", () => {
   test('Navigate to baseurl and login into system', async ({ signinPage }) => {
-    test.setTimeout(180000);
+    test.setTimeout(60000);
     await signinPage.navigateTo("https://dev.scriptsense.co.nz/");
     await signinPage.loginForm(process.env.LOGIN_USERNAME, process.env.LOGIN_PASSWORD);
+    await console.log("login successful");
   });
+
 
   test("Search patient and create a manual dispense", async ({ homePage }) => {
     await homePage.disablePrinter();
